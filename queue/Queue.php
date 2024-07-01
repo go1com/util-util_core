34378650681306726651a5df8e899e2ec0e11e06
An Hoang <an.hoang@go1.com>
2024-07-01 13:13:21 +0700

    To mark a constant `DO_MAIL_BULK_SEND` as deprecated

--- queue/Queue.php
+++ queue/Queue.php
@@ -315,6 +315,7 @@ class Queue
     public const DO_FINDER                            = 'do.finder';
     public const DO_PUBLIC_API_WEBHOOK_REQUEST        = 'do.public-api.webhook-request'; # { appId: INT, url: STRING, subject: OBJECT, original: null|OBJECT }
     public const DO_MAIL_SEND                         = 'do.mail.send'; # { subject: STRING, body: STRING, html: STRING, context: OBJECT, attachments: STRING[], options: OBJECT }
+    // @deprecated
     public const DO_MAIL_BULK_SEND                    = 'mail-bulk.send'; # { subject: STRING, body: STRING, html: STRING, context: OBJECT, attachments: STRING[], options: OBJECT }
     public const DO_HISTORY_RECORD                    = 'do.history.record';
     public const DO_ENROLMENT                         = 'process.enrolment'; # { action: STRING, body: OBJECT }
